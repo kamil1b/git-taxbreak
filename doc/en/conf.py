8a018d64130e0b10cee2217eb10fa36020bce965
Kamil Luczak <kamilluczak@luczakweb.pl>
2020-03-15 13:21:50 +0100

    Add pylint disable for conf.py

--- doc/en/conf.py
+++ doc/en/conf.py
@@ -1,22 +1,11 @@
 # -*- coding: utf-8 -*-
-#
-# Configuration file for the Sphinx documentation builder.
-#
-# This file does only contain a selection of the most common options. For a
-# full list see the documentation:
-# http://www.sphinx-doc.org/en/master/config
-
+# pylint: disable=invalid-name,redefined-builtin,missing-docstring
 # -- Path setup --------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../../src/"))
-
 
 # -- Project information -----------------------------------------------------
 
